b03c0acae90f51ac902c7ce6db00cceab78678dd
Krishna Sonani <krishnasonani11@gmail.com>
2023-06-09 14:53:41 -0400

    My First Commit...

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c); 
+	}
+
+}
